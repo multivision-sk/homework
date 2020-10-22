abc6860f8887d6f9e32ea9ccb4720ade9c9f18a3
Seulki <67934680+multivision-sk@users.noreply.github.com>
2020-10-22 18:47:04 +0900

    Update hw_week03_skk.py

--- hw_week03_skk.py
+++ hw_week03_skk.py
@@ -7,7 +7,7 @@ db = client.dbmusic
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# 아래 빈 칸('')을 채워보세요
+
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20201016', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
